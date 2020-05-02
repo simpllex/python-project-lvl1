36c105d9b66d31960283bc34052899268a906aa0
vera <marinenskaya.vera@gmail.com>
2020-05-02 23:23:53 +0400

    add asciinema in readme

---
+++ brain_games/games/prime.py
@@ -0,0 +1,43 @@
+from random import randint
+import math
+import prompt
+
+
+def rules_game():
+    print('Welcome to the Brain Games!')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    name = prompt.string('May I have your name? ')
+    print("Hello, {}!".format(name))
+    return name
+
+
+def isPrime(number):
+    if number < 2:
+        return "no"
+    if number ==  2:
+        return "yes"
+    limit = int(math.sqrt(number))
+    i = 2
+    while i <= limit:
+        if number % i == 0:
+            return "no"
+        i += 1
+    return "yes"
+
+
+def user_quest(name):
+    count_answer = 0
+    while count_answer < 3:
+        random_number = randint(0, 100)
+        correct_answer = isPrime(random_number)
+        user_answer = prompt.string("Question: {} ".format(random_number))
+        if user_answer == correct_answer:
+            print('Correct!')
+            count_answer += 1
+        else:
+            print("'{}' is wrong answer ;(. Correct answer was '{}'.\n"
+                  "Let's try again, {}!".format(user_answer,
+                                                correct_answer, name))
+            break
+    if count_answer == 3:
+        print("Congratulations, {}!".format(name))
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.games import prime
+
+
+def main():
+    name = prime.rules_game()
+    prime.user_quest(name)
+
+
+if __name__ == '__main__':
+    main()
